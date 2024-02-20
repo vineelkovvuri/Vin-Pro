157b387119fc88ebb1aa4ef083cf50d24fc35186
Vineel Kovvuri <vineel.kovvuri@gmail.com>
2024-02-19 16:32:36 -0800

    Fixups
    Signed-off-by: Vineel Kovvuri <vineel.kovvuri@gmail.com>

---
+++ Rust/src/1.fahren_to_centi.rs
@@ -0,0 +1,9 @@
+
+
+fn main()
+{
+    let fahren = 10;
+    let centi = (fahren as f32 - 32.0)/1.8;
+
+    println!("{}", centi);
+}
---
+++ Rust/src/2.sum_and_avg_3_nums.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let a = 10;
+    let b = 20;
+    let c = 40;
+
+    let result = (a + b + c) as f32 / 3.0;
+
+    println!("{result}");
+}
