062f7a8561150cda0b21a8711a3794e6aee61752
Vineel Kovvuri <vineel.kovvuri@gmail.com>
2024-05-25 01:35:42 -0700

    Fixups
    Signed-off-by: Vineel Kovvuri <vineel.kovvuri@gmail.com>

--- Rust/Search/src/main.rs
+++ Rust/Search/src/main.rs
@@ -1,24 +1,24 @@
 use std::fs;
 use std::path::Path;
 
-struct ExactFileNameMatcher<'a> {
-    file_name: &'a str,
-}
+// struct ExactFileNameMatcher<'a> {
+//     file_name: &'a str,
+// }
 
-trait Matcher {
-    fn match_path(&self, file_path: &str) -> bool;
-}
+// trait Matcher {
+//     fn match_path(&self, file_path: &str) -> bool;
+// }
 
-impl<'a> Matcher for ExactFileNameMatcher<'a> {
-    fn match_path(&self, file_path: &str) -> bool {
-        file_path.ends_with(self.file_name)
-    }
-}
+// impl<'a> Matcher for ExactFileNameMatcher<'a> {
+//     fn match_path(&self, file_path: &str) -> bool {
+//         file_path.ends_with(self.file_name)
+//     }
+// }
 
 fn main() {
     let mut results: Vec<String> = Vec::new();
     let path = Path::new("C:\\");
-    search(&path, "kernel32.dll", &mut results);
+    search2(&path, "kernel32.dll", &mut results);
     for result in results {
         println!("{:?}", result);
     }
@@ -37,6 +37,29 @@ fn search(path: &Path, file_name: &str, results: &mut Vec<String>) {
                     results.push(String::from(sub_path.as_os_str().to_str().unwrap()));
                 }
             }
+        }
+    }
+}
+
+fn search2(path: &Path, file_name: &str, results: &mut Vec<String>) {
+    let result = fs::read_dir(&path);
+    if result.is_err() {
+        return;
+    }
+
+    let entries = result.unwrap();
+    for entry in entries {
+        if entry.is_err() {
+            continue;
+        }
+
+        let entry = entry.unwrap();
+        let sub_path = entry.path();
+        if sub_path.is_dir() {
+            let sub_path = Path::new(&sub_path);
+            search(sub_path, file_name, results);
+        } else if sub_path.ends_with(file_name) {
+            results.push(String::from(sub_path.as_os_str().to_str().unwrap()));
         }
     }
 }
