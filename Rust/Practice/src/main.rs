8e2c9f5a2de766f2069e7b17d2f953cfb7e95a91
Vineel Kovvuri <vineel.kovvuri@gmail.com>
2024-08-24 22:55:06 -0700

    Fixups
    Signed-off-by: Vineel Kovvuri <vineel.kovvuri@gmail.com>

--- Rust/Practice/src/main.rs
+++ Rust/Practice/src/main.rs
@@ -1,21 +1,54 @@
+use std::{
+    cell::RefCell,
+    ops::{Deref, DerefMut},
+};
 
-struct Book {
-    pages: u32, // immutable
-    cost: u32,  // can be mutated
+trait Shape {
+    fn area(&self);
 }
 
-impl From<u32> for Book {
-    fn from(value: u32) -> Self {
-        Book {
-            pages: value,
-            cost: value,
-        }
+struct Circle {
+    r: u32,
+}
+
+impl Shape for Circle {
+    fn area(&self) {}
+}
+
+struct Rectangle {
+    l: u32,
+    b: u32,
+}
+
+impl Shape for Rectangle {
+    fn area(&self) {}
+}
+
+struct Shaper<T> {
+    shape: T,
+}
+
+impl<T> Deref for Shaper<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.shape
     }
 }
-fn main() {
-    let arr = [0u8; 10];
-    let s = arr.split_at(5);
-    let s1 = s.0.split_at(5);
 
-    let v = vec![1, 2, 3, 4];
+impl<T> DerefMut for Shaper<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.shape
+    }
+
+fn main() {
+    let shaper = Shaper {
+        shape: Circle { r: 20 },
+    };
+    shaper.area();
+
+    let shaper = Shaper {
+        shape: Rectangle { l: 20, b: 30 },
+    };
+    shaper.area();
 }
