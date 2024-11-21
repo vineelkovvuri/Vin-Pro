24d165f487d03bff62853652f53a46bfac1e2961
Vineel Kovvuri <vineel.kovvuri@gmail.com>
2024-11-21 14:21:31 -0800

    more and more rust
    Signed-off-by: Vineel Kovvuri <vineel.kovvuri@gmail.com>

--- Rust/Practice/src/59.map_or_impl.rs
+++ Rust/Practice/src/59.map_or_impl.rs
@@ -1,34 +1,32 @@
 // map_or
-// map_or_else (() -> U, (T) -> U) -> U
+// map_or (U, (T) -> U) -> U
 //    Some(T) => (T) -> U
-//    None    => () -> U
+//    None    => U
 fn main() {
     let x = Some(20);
-    dbg!(x.map_or_else(|| 11.2, |x| 10.2)); // 10.2
+    dbg!(x.map_or(11.2, |x| 10.2)); // 10.2
 
     // We should prefix Option::<u32>:: without which the compiler cannot infer
     // the type from map(), unlike other unwrap functions
     let x = Option::<u32>::None;
-    dbg!(x.map_or_else(|| 11.2, |x| 10.2)); // 11.2
+    dbg!(x.map_or(11.2, |x| 10.2)); // 11.2
 
     let x = Some(20);
-    dbg!(my_map_or_else(x, || 11.2, |x| 10.2)); // 10.2
+    dbg!(my_map_or(x, 11.2, |x| 10.2)); // 10.2
 
     let x = Option::<u32>::None;
-    dbg!(my_map_or_else(x, || 11.2, |x| 10.2)); // 11.2
+    dbg!(my_map_or(x, 11.2, |x| 10.2)); // 11.2
 }
 
 // This function will map T -> U in the follow way
 //    Some(T) => (T) -> U
-//    None    => () -> U
-// This is just lazy version of map_or()
-fn my_map_or_else<T, D, F, U>(opt: Option<T>, d: D, f: F) -> U
+//    None    => U
+fn my_map_or<T, F, U>(opt: Option<T>, default: U, f: F) -> U
 where
-    D: FnOnce() -> U,
     F: FnOnce(T) -> U,
 {
     match opt {
         Some(value) => f(value),
-        None => d(),
+        None => default,
     }
 }
