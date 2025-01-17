0262b925a9b69ee1da891a887f8578a4261df95e
Vineel Kovvuri <vineel.kovvuri@gmail.com>
2025-01-16 22:04:39 -0800

    Update dotconfig paths
    Signed-off-by: Vineel Kovvuri <vineel.kovvuri@gmail.com>

--- dotconfig/windows/ConfigureMachine.ps1
+++ dotconfig/windows/ConfigureMachine.ps1
@@ -2,23 +2,25 @@
 
 Set-ExecutionPolicy Unrestricted;
 
-Function AppendPathVariable
-{
+Function AppendPathVariable {
     param($Path = $null)
 
     $UserPath = [Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User);
     $SystemPath = [Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine);
 
-    if (!$UserPath.Contains($Path.ToLower()) -and !$SystemPath.Contains($Path.ToLower()))
-    {
+    if (!$UserPath.Contains($Path.ToLower()) -and !$SystemPath.Contains($Path.ToLower())) {
         [Environment]::SetEnvironmentVariable("Path", $UserPath + ";$($Path)", [System.EnvironmentVariableTarget]::User)
     }
 }
 
 [Environment]::SetEnvironmentVariable("_NT_SOURCE_PATH", "SRV*C:\Sources", [System.EnvironmentVariableTarget]::User)
 [Environment]::SetEnvironmentVariable("_NT_SYMBOL_PATH", "SRV*C:\Symbols*https://symweb.azurefd.net;SRV*C:\Symbols*https://msdl.microsoft.com/download/symbols", [System.EnvironmentVariableTarget]::User)
+[Environment]::SetEnvironmentVariable("YAZI_FILE_ONE", "C:\Program Files\Git\usr\bin\file.exe", [System.EnvironmentVariableTarget]::User)
+
 AppendPathVariable -Path "$($env:USERPROFILE)\AppData\Local\DBG\UI"
 AppendPathVariable -Path "$($env:USERPROFILE)\OneDrive\Softs\Tools\rg"
+AppendPathVariable -Path "$($env:USERPROFILE)\OneDrive\Softs\Tools\WezTerm"
+AppendPathVariable -Path "$($env:USERPROFILE)\OneDrive\Softs\Tools\poppler"
 AppendPathVariable -Path "C:\Program Files\Git\usr\bin"
 AppendPathVariable -Path "C:\Program Files\Git\cmd"
 AppendPathVariable -Path "C:\Program Files\Notepad4"
@@ -26,4 +28,6 @@ AppendPathVariable -Path "C:\Program Files\Sublime Text"
 AppendPathVariable -Path "C:\Program Files\eSpeak NG"
 AppendPathVariable -Path "C:\Program Files (x86)\Meld\meld"
 AppendPathVariable -Path "C:\msys64\ucrt64\bin"
+AppendPathVariable -Path "C:\Program Files\GitHub CLI\"
+
 
